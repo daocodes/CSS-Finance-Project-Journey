2583f55abefe37b3b0d916a248b42451ea6346a6
Dao Bui <tdaobui2025@gmail.com>
2025-07-18 16:06:08 -0400

    App Developments July 18,2025

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        big: ["Plus Jakarta Sans"],
+        small: ["Inter"],
+      }
+
+    },
+  },
+  plugins: [],
+}
+
